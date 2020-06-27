758bd72abda30d4ec72112e58a859974739e5559
向鲜汶 <xiangxianwen@dianmi365.com>
2020-06-27 16:25:23 +0800

    demo

---
+++ lib/project/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(Main());
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
---
+++ lib/project/widget/XWTAbBarState.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class _XWTabBarState extends State with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
